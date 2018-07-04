0220164ddb11daefa79f3ee198e67fe8df50b4f0
sande2jm <sande2jm@gmail.com>
2018-07-04 03:41:46 -0400

    version 4.1

--- worker_json.py
+++ worker_json.py
@@ -60,7 +60,7 @@ class Worker():
 	def convert_json(self):
 		print(type(self.data['images']),self.data['images'][0], self.data['images'][0]['url'])
 		num_cores = multiprocessing.cpu_count()
-		images = Parallel(n_jobs=num_cores)(delayed(self.create_image)(i) for i in self.data['images'][10])
+		images = Parallel(n_jobs=num_cores)(delayed(self.create_image)(i) for i in self.data['images'][:10])
 		return images
 
 	def create_image(self,elem):
