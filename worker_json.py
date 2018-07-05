90f3f262b247230697ce1cda9726fd1f7c6bc9ae
sande2jm <sande2jm@gmail.com>
2018-07-05 04:44:27 -0400

    version 8.8

--- worker_json.py
+++ worker_json.py
@@ -65,7 +65,8 @@ class Worker():
 		# results = [self.create_image(x) for x in self.data['images']]
 		results = []
 		for i,x in enumerate(self.data['images']):
-			if i %100 == 0:self.report(i)
+			if i %100 == 0:
+				self.report(i)
 			results.append(self.create_image(x))
 		return results
 
