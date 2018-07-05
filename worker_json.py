0e3575d4e6879f34f056f8ae62019caabd95fdd7
sande2jm <sande2jm@gmail.com>
2018-07-05 00:23:04 -0400

    version 7.8

--- worker_json.py
+++ worker_json.py
@@ -64,11 +64,11 @@ class Worker():
 		# print(type(self.data['images']),self.data['images'][0], self.data['images'][0]['url'])
 		#num_cores = multiprocessing.cpu_count()
 		# images = Parallel(n_jobs=num_cores)(delayed(self.create_image)(i) for i in self.data['images'][:100])
-		dummy = self.data['images'][:]
+		dummy = []
 		results = []
 		num_cores = multiprocessing.cpu_count()
 		print(num_cores)
-		print(type(self.data['images']))
+		print(type(self.data['images'][0]))
 		if num_cores > 1:
 			results = Parallel(n_jobs=num_cores)(delayed(self.create_image)(i) for i in dummy)
 		else:
