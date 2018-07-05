a64fdb3ca35275bb67aec1baf43e7fb0c384e871
sande2jm <sande2jm@gmail.com>
2018-07-04 20:35:14 -0400

    version 6.4

--- launch.py
+++ launch.py
@@ -5,11 +5,21 @@ import json
 from worker_json import Worker
 import sys
 
+sqs = boto3.resource('sqs')
+# Create the queue. This returns an SQS.Queue instance
+queue = sqs.get_queue_by_name(QueueName='test')
+my_id = check_output(['curl', 'http://169.254.169.254/latest/meta-data/instance-id'])
+my_id = "".join(map(chr, self.my_id))
 
-print("Launch Complete")
-sys.stdout.flush()
-w = Worker()
-w.extract()
-w.run()
-w.report()
+d = {
+'message': 'launched',
+'id': my_id,
+'progress': 'None'}
+
+response = queue.send_message(MessageBody=json.dumps(d))
+
+# w = Worker()
+# w.extract()
+# w.run()
+# w.report()
 
